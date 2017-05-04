79237eccc69a63f6b9165e9b0879b5bf76a334e7
ariazdev02 <ariaz.dev02@gmail.com>
2017-05-04 14:48:40 +0430

    change structure of 404.php and page.php

--- 404.php
+++ 404.php
@@ -1,2 +1,12 @@
-<?php
-echo '404 error';
+<?php get_header();
+if(have_posts()):
+    while(have_posts()):
+    the_post();
+
+    endwhile;
+endif;
+?>
+<div class="container">
+<?php get_footer(); ?>
+
+    
