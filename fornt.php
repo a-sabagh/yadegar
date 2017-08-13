8b7a4778495f62ed18d989abc27c5a48d57a0b34
ariazdev02 <ariaz.dev02@gmail.com>
2017-08-13 08:46:00 +0430

    adding content-product part

--- fornt.php
+++ fornt.php
@@ -7,14 +7,17 @@ get_header();
 ?>
 <div class="container">
     <div class="row">
-        <?php get_template_part('templates/content' , 'intro'); ?>
+        <?php get_template_part('templates/content', 'intro'); ?>
         <div class="vertical-space-4"></div><!--.vertical-space-4-->
         <!--#############################################################################################-->
-        <?php get_template_part('templates/blog' , 'squre'); ?>
+        <?php get_template_part('templates/blog', 'squre'); ?>
         <div class="vertical-space-4"></div><!--.vertical-space-4-->
         <!--#############################################################################################-->
-        <?php get_template_part('templates/qa' , 'tab'); ?>
+        <?php get_template_part('templates/content', 'product'); ?>
         <div class="vertical-space-4"></div><!--.vertical-space-4-->
+        <!--#############################################################################################-->
+        <?php /* get_template_part('templates/qa' , 'tab'); */ ?>
+        <!--<div class="vertical-space-4"></div>.vertical-space-4-->
         <!--#############################################################################################-->
     </div><!--.row-->
     <?php get_footer(); ?>
