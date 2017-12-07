9329b0ae5855bcfdf6170fd8362e7479f836e11c
asabagh <a.sabagh72@gmail.com>
2017-12-07 12:14:24 +0330

    show title or not with page metaboxes

--- page.php
+++ page.php
@@ -3,18 +3,20 @@ get_header();
 if (have_posts()):
     while (have_posts()):
         the_post();
-        ?>
-        <div class="row text-center">
-            <h3 class="section-title"><?php the_title(); ?></h3>
-            <p class="section-caption"></p>
-        </div>
-        <?php
-        echo '<div class="container">';
+        if (get_post_meta(get_the_ID(), "rng_show_title", TRUE) == 'on'):
+            ?>
+            <div class="text-center">
+                <h3 class="section-title"><?php the_title(); ?></h3>
+            </div>
+            <?php
+        endif;
+        echo '<div class="container page-content">';
         the_content();
         echo '</div>';
     endwhile;
 endif;
 ?>
+<div class="vertical-space-4"></div><!--.vertical-space-4-->
 <div class="container">
     <?php get_footer(); ?>
 
